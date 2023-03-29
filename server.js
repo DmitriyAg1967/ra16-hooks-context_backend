85063ebc29b04e9214ff91fb62ca3146d6ee6836
Dmitriy <aglerimov05@mail.ru>
2023-03-29 21:40:41 +0300

    Третий коммит

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 const http = require('http');
 const Koa = require('koa');
-const Router = require('koa-router');
-const cors = require('koa2-cors');
 const koaBody = require('koa-body');
-const console = require('console');
+const cors = require('koa2-cors');
+const Router = require('koa-router');
 
 const app = new Koa();
+
+app.use(koaBody({
+  text: true,
+  urlencoded: true,
+  multipart: true,
+  json: true,
+}));
+
 app.use(cors());
-app.use(koaBody());
 
 const router = new Router();
+
 router.get('/data', async (ctx, next) => {
-    ctx.response.body = { status: "ok" };
-});
-router.get('/error', async (ctx, next) => {
-    ctx.response.status = 500;
-    ctx.response.body = { status: "Internal Error" };
-});
-router.get('/loading', async (ctx, next) => {
-    await new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-        }, 5000);
-    });
-    ctx.response.body = { status: "ok" };
+  ctx.response.body = {status: "ok"};
 });
 
-app.use(router.routes())
-app.use(router.allowedMethods());
+router.get('/error', async (ctx, next) => {
+  ctx.response.status = 500;
+  ctx.response.body = {status: "Internal Error"};
+});
+
+router.get('/loading', async (ctx, next) => {
+  await new Promise(resolve => {
+      setTimeout(() => {
+          resolve();
+      }, 5000);
+  });
+  ctx.response.body = {status: "ok"};
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
-server.listen(port);
-console.log('server listen port 7070')
+server.listen(port, () => console.log(`The server started on port ${port}`));
+console.log(`http://localhost:${port}`)
